4f5a437811cd54ef1492e91d3768116e656e623f
Mazharul Islam <swopnil420@gmail.com>
2021-10-03 06:13:59 +0600

    added logo, navbar and login button on header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import Header from './Components/Header/Header';
 
 function App() {
 	return (
 		<div className='App'>
 			<Router>
+				<Header />
 				<Switch>
 					<Route exact path='/'>
 						<Login />
